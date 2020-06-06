e019ec7177f64a1302d99a11b7695dee211748b0
habibasiddiqui <habiba_siddiqui@ymail.com>
2020-06-06 20:55:32 +0500

    my website ready

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// CAREER JOB COLLAPSE ARROWS
+$(document).ready(function(){
+    $('.arrow').on("click", function (event) {
+        $('.arrow-img').toggleClass('rotate');
+        $('.arrow-img').toggleClass('rotate2');
+    });
+});
+$('.arrow').on("click", function (event) {
+    $('.arrow-img').toggleClass('rotate');
+    $('.arrow-img').toggleClass('rotate2');
+});
+
+$(document).ready(function(){
+    $('.arrowTwo').on("click", function (event) {
+        $('.arrowTwo-img').toggleClass('rotateTwo');
+        $('.arrowTwo-img').toggleClass('rotate2Two');
+    });
+});
+$('.arrowTwo').on("click", function (event) {
+    $('.arrowTwo-img').toggleClass('rotateTwo');
+    $('.arrowTwo-img').toggleClass('rotate2Two');
+});
+
+$(document).ready(function(){
+    $('.arrowThree').on("click", function (event) {
+        $('.arrowThree-img').toggleClass('rotateThree');
+        $('.arrowThree-img').toggleClass('rotate2Three');
+    });
+});
+$('.arrowThree').on("click", function (event) {
+    $('.arrowThree-img').toggleClass('rotateThree');
+    $('.arrowThree-img').toggleClass('rotate2Three');
+});
+
+$(document).ready(function(){
+    $('.arrowFour').on("click", function (event) {
+        $('.arrowFour-img').toggleClass('rotateFour');
+        $('.arrowFour-img').toggleClass('rotate2Four');
+    });
+});
+$('.arrowFour').on("click", function (event) {
+    $('.arrowFour-img').toggleClass('rotateFour');
+    $('.arrowFour-img').toggleClass('rotate2Four');
+});
+
+$(document).ready(function(){
+    $('.arrowFive').on("click", function (event) {
+        $('.arrowFive-img').toggleClass('rotateFive');
+        $('.arrowFive-img').toggleClass('rotate2Five');
+    });
+});
+$('.arrowFive').on("click", function (event) {
+    $('.arrowFive-img').toggleClass('rotateFive');
+    $('.arrowFive-img').toggleClass('rotate2Five');
+});
+
